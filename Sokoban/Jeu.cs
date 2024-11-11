07f8f85a71c9adc02736c16faf6daa06bf8d1844
Joala ANDRIAMAHADY RATSIMAMENDRIKAJA <joala@corp.telma.mg>
2024-11-11 10:10:58 +0300

    sokoban: v1.0.0

---
+++ Sokoban/Jeu.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sokoban
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Jeu
+    {
+        public enum Etat
+        {
+            Mur,
+            Cible,
+            Vide
+        }
+
+        public Etat[,] grille;
+
+        public List<Position> caisse;
+
+        public Position personnage;
+
+        private int nbrDepla;
+
+        static String grilleTxt = "..XXXXXX..XXX.oo.XXXX..o..o..XX........XXXX....XXX..XX.CXX...XXXC.XXX..X.CP.C.X..X......X..XXXXXXXX.";
+
+        public int NbrDepla
+        {
+            get
+            {
+                return nbrDepla;
+            }
+        }
+
+        public Jeu()
+        {
+            grille = new Etat[10, 10];
+            initJeu();
+            nbrDepla = 0;
+        }
+
+        private void initJeu()
+        {
+            caisse = new List<Position>();
+
+            for (int ligne = 0; ligne < 10; ligne++)
+            {
+                for (int colonne = 0; colonne < 10; colonne++)
+                {
+                    switch (grilleTxt[ligne * 10 + colonne])
+                    {
+                        case '.':
+                            grille[ligne, colonne] = Etat.Vide;
+                            break;
+                        case 'X':
+                            grille[ligne, colonne] = Etat.Mur;
+                            break;
+                        case 'o':
+                            grille[ligne, colonne] = Etat.Cible;
+                            break;
+                        case 'C':
+                            caisse.Add(new Position(ligne, colonne));
+                            grille[ligne, colonne] = Etat.Vide;
+                            break;
+                        case 'P':
+                            personnage = new Position(ligne, colonne);
+                            grille[ligne, colonne] = Etat.Vide;
+                            break;
+                    }
+                }
+            }         
+        }
+
+        public void ToucheAppyer(Key key)
+        {
+            Position newPos = new Position(personnage.x, personnage.y);
+
+            CalculNewPos(newPos, key);
+            if (CaseOK(newPos, key))
+            {
+                personnage = newPos;
+                nbrDepla++;
+            }
+
+        }
+
+        public bool Fini()
+        {
+            foreach (Position caiss in caisse)
+            {
+                if (grille[caiss.x, caiss.y] != Etat.Cible)
+                {
+                    return false;
+                }              
+            }
+            return true;
+        }
+
+        private static void CalculNewPos(Position newPos, Key key)
+        {
+            switch (key)
+            {
+                case Key.Down:
+                    newPos.x++;
+                    break;
+                case Key.Up:
+                    newPos.x--;
+                    break;
+                case Key.Right:
+                    newPos.y++;
+                    break;
+                case Key.Left:
+                    newPos.y--;
+                    break;
+            }
+        }
+
+        private bool CaseOK(Position newPos, Key key)
+        {
+            // Presence d'un mur
+            if (grille[newPos.x, newPos.y] == Etat.Mur)
+            {
+                return false;
+            }
+
+            Position caisse = caisseInPos(newPos);
+
+            if ( caisse != null)
+            {
+                // Deplacement possible de la caisse
+                Position newPositionCaisse = new Position(caisse.x, caisse.y);
+                CalculNewPos(newPositionCaisse, key);
+
+                if (grille[newPositionCaisse.x, newPositionCaisse.y] == Etat.Mur)
+                {
+                    return false;
+                }
+                else if (caisseInPos(newPositionCaisse) != null)
+                {
+                    return false;
+                }
+                else
+                {
+                    caisse.x = newPositionCaisse.x;
+                    caisse.y = newPositionCaisse.y;
+                    return true;
+                }
+
+            }
+            return true;
+
+
+            // Presence d'une caisse sur la case ou le personnage va y aller
+        }
+
+        public void Restart()
+        {
+            initJeu();
+            nbrDepla = 0;
+        }
+
+        private Position caisseInPos(Position newPos)
+        {
+            foreach (Position caissBois in caisse)
+            {
+                if (caissBois.x == newPos.x && caissBois.y == newPos.y)
+                {
+                    return caissBois;
+                }
+            }
+            return null;
+        }
+
+        public Etat Case(int ligne, int colonne)
+        {
+            return grille[ligne, colonne];
+        }
+    }
+}
---
+++ Sokoban/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sokoban
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Jeu jeu;
+        public MainWindow()
+        {
+            InitializeComponent();
+            jeu = new Jeu();
+            this.KeyDown += MainWindow_KeyDown;
+            Dessiner();//DessinerCarte
+            Redessiner();//Dessiner caisse et perso
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Right) || e.Key.Equals(Key.Left) || e.Key.Equals(Key.Up) || e.Key.Equals(Key.Down))
+            {
+                jeu.ToucheAppyer(e.Key);
+                Redessiner();
+            }
+            if (jeu.Fini())
+            {
+                MessageBoxResult msg = MessageBox.Show("Congratulation! You win.");
+            }
+        }
+
+        private void Redessiner()
+        {
+            cnvMobile.Children.Clear();
+            DessinerCaisse();
+            DessinerPerso();
+            AfficherNbr();
+        }
+
+        private void AfficherNbr()
+        {
+            txtMoveNumber.Text = jeu.NbrDepla.ToString();
+        }
+
+        private void DessinerPerso()
+        {
+            Rectangle r = new Rectangle();
+            r.Width = 50;
+            r.Height = 50;
+            r.Margin = new Thickness(jeu.personnage.y *50, jeu.personnage.x *50, 0, 0);
+            r.Fill = new ImageBrush(new BitmapImage(new Uri("images/character.png", UriKind.Relative)));
+            cnvMobile.Children.Add(r);
+        }
+
+        private void DessinerCaisse()
+        {
+            foreach (Position pos in jeu.caisse)
+            {
+                Rectangle r = new Rectangle();
+                r.Width = 42;
+                r.Height = 42;
+                r.Margin = new Thickness(pos.y *50+4, pos.x *50+4, 0, 0);
+                r.Fill = new ImageBrush(new BitmapImage(new Uri("images/box.png", UriKind.Relative)));
+                cnvMobile.Children.Add(r);
+            }
+        }
+
+        private void Dessiner()
+        {
+            DessinerCarte();
+        }
+
+        private void DessinerCarte()
+        {
+            for (int ligne = 0; ligne < 10; ligne++)
+            {
+                for (int colonne = 0; colonne < 10; colonne++)
+                {
+                    Rectangle r = new Rectangle();
+                    r.Width = 50;
+                    r.Height = 50;
+                    r.Margin = new Thickness(colonne * 50, ligne * 50, 0, 0);
+
+                    switch (jeu.Case(ligne, colonne))
+                    {
+                        case Jeu.Etat.Vide:
+                            r.Fill = new ImageBrush(new BitmapImage(new Uri("images/vide.jpg", UriKind.Relative)));
+                            break;
+                        case Jeu.Etat.Mur:
+                            r.Fill = new ImageBrush(new BitmapImage(new Uri("images/mur.jpg", UriKind.Relative)));
+                            break;
+                        case Jeu.Etat.Cible:
+                            r.Fill = new ImageBrush(new BitmapImage(new Uri("images/diamond_2.png", UriKind.Relative)));
+                            break;
+                    }
+                    cnvGrille.Children.Add(r);
+                }
+            }
+        }
+
+        private void btnRetry_Click(object sender, RoutedEventArgs e)
+        {
+            jeu.Restart();
+            Redessiner();
+        }
+    }
+}
